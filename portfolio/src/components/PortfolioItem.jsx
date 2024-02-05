919480c3a991dee39bea3a05f11ca4665de860ab
Caio Vinicius Aguiar Alves <caiovini.aa@gmail.com>
2024-02-05 09:22:16 -0300

    feat: implemented dark mode, contact, social icons and finished the visuals

--- portfolio/src/components/PortfolioItem.jsx
+++ portfolio/src/components/PortfolioItem.jsx
@@ -7,7 +7,7 @@ function PortfolioItem({ title, imageUrl, stack, link }) {
             target="_blank"
             rel="noreferrer noopener"
         >
-            <div className="border-2 border-stone-900 rounded-md overflow-hidden">
+            <div className="border-2 border-stone-900 dark:border-stone-200 rounded-md overflow-hidden">
                 <img 
                     src={ imageUrl } 
                     alt={ title }
@@ -15,10 +15,10 @@ function PortfolioItem({ title, imageUrl, stack, link }) {
                 />
 
                 <div className="x-full p-4">
-                    <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">{ title }</h3>
-                    <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
+                    <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold dark:text-stone-200">{ title }</h3>
+                    <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-stone-200">
                         { stack.map(item => (
-                            <span className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 rounded-md">
+                            <span className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 dark:border-stone-200 rounded-md">
                                 { item }
                             </span>
                         )) }
--- portfolio/src/components/Timeline.jsx
+++ portfolio/src/components/Timeline.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import TimelineData from "../data/TimelineData";
 import TimelineItem from "./TimelineItem";
+import Title from "./Title";
 
 function Timeline() {
     return (
         <div className="flex flex-col md:flex-row justify-center my-20">
             <div className="w-full md:w-7/12">
+                <Title>Timeline</Title>
                 {TimelineData.map(item => (
                     <TimelineItem
                         year={ item.Year }
